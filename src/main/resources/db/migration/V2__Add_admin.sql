d2b0a41e875fa3a21e8148836c9cb59b0e22733f
Chrome-zZ <spiderzhum@mail.ru>
2021-07-11 12:53:29 +0300

    Added FlyWay

---
+++ src/main/resources/db/migration/V2__Add_admin.sql
@@ -0,0 +1,2 @@
+insert into usr (automobile, password, role, status, username)
+    VALUES ('-', '$2y$12$7MEDCiFZIziGJDDXAzBMA.nyebPvW7ayB8US4ZDfl86ViTGibTaFu', 'ADMIN', 'ACTIVE', 'Admin')
---
+++ src/main/resources/db/migration/V3__Add_test_points.sql
@@ -0,0 +1,34 @@
+insert into usr (automobile, password, role, status, username)
+VALUES ('Some car', '$2y$12$qHTJxO8lIwByC78GGN9ja.cs4QwnXjxKdhmrGunvQfqNY3tdlFt12', 'User', 'ACTIVE', 'Alex'),
+       ('Some car', '$2y$12$qHTJxO8lIwByC78GGN9ja.cs4QwnXjxKdhmrGunvQfqNY3tdlFt12', 'User', 'ACTIVE', 'Adele');
+
+insert into facilities (name, price)
+VALUES ('WashCar', 100),
+       ('PolishCar', 200),
+       ('LuxWash', 300);
+
+insert into schedules (free, time)
+VALUES (true, '00:00:00'),
+       (true, '01:00:00'),
+       (true, '02:00:00'),
+       (true, '03:00:00'),
+       (true, '04:00:00'),
+       (true, '05:00:00'),
+       (true, '06:00:00'),
+       (true, '07:00:00'),
+       (true, '08:00:00'),
+       (true, '09:00:00'),
+       (true, '10:00:00'),
+       (true, '11:00:00'),
+       (true, '12:00:00'),
+       (true, '13:00:00'),
+       (true, '14:00:00'),
+       (true, '15:00:00'),
+       (true, '16:00:00'),
+       (true, '17:00:00'),
+       (true, '18:00:00'),
+       (true, '19:00:00'),
+       (true, '20:00:00'),
+       (true, '21:00:00'),
+       (true, '22:00:00'),
+       (true, '23:00:00')
